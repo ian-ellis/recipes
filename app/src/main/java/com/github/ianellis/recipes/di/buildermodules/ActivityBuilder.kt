c96855638bac87d0b346b6d4220c6f7e887bf6d2
Ian Ellis <ian.ellis@theiconic.com.au>
2018-06-03 11:16:57 +1000

    Adding Activity Builder module
    This class helps us define the root of our Di graph. In this instance the root of a graph is out Recipes activity. Here we provide out ViewModelsModule and GetRecipesModule annotated with Activity scope meaning these dependencies will have a scope associated with the Activity

---
+++ app/src/main/java/com/github/ianellis/recipes/di/buildermodules/ActivityBuilder.kt
@@ -0,0 +1,19 @@
+package com.github.ianellis.recipes.di.buildermodules
+
+import com.github.ianellis.recipes.di.modules.ViewModelModule
+import com.github.ianellis.recipes.domain.common.di.scopes.ActivityScope
+import com.github.ianellis.recipes.domain.getrecipes.di.GetRecipesModule
+import com.github.ianellis.recipes.presentation.recipes.RecipesActivity
+import dagger.Module
+import dagger.android.ContributesAndroidInjector
+
+@Module
+abstract class ActivityBuilder {
+
+  @ActivityScope
+  @ContributesAndroidInjector(modules = [
+    ViewModelModule::class,
+    GetRecipesModule::class
+  ])
+  abstract fun bindRecipesActivity(): RecipesActivity
+}
