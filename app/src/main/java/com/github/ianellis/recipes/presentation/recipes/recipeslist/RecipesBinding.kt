502af7d5e774e45c44ec1f3e69ada62dd5b0e014
Ian Ellis <ian.ellis@theiconic.com.au>
2018-06-03 19:05:39 +1000

    Adding RecyclerView to RecipesActivity View
    In order to bind our view model to our recycler we need to add a custom binding. Because this binding is specific to our custom view we have implemented our own binding class inside the component rather than adding to the already common binding object

---
+++ app/src/main/java/com/github/ianellis/recipes/presentation/recipes/recipeslist/RecipesBinding.kt
@@ -0,0 +1,15 @@
+package com.github.ianellis.recipes.presentation.recipes.recipeslist
+
+import android.databinding.BindingAdapter
+import com.github.ianellis.recipes.domain.getrecipes.Recipe
+import com.github.ianellis.recipes.presentation.recipes.recipeslist.RecipesRecycler
+
+object RecipesBinding {
+
+  @JvmStatic
+  @BindingAdapter("recipes")
+  fun bindRecipes(recycler: RecipesRecycler, recipes: List<Recipe>?) {
+    recycler.setRecipes(recipes ?: emptyList())
+  }
+
+}
