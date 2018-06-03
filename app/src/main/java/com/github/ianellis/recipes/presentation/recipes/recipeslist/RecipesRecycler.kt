b9a2691d3caa5af558c3f7a3992f416fa4d91c69
Ian Ellis <ian.ellis@theiconic.com.au>
2018-06-03 19:04:07 +1000

    Adding RecipesRecycler
    This will be our custom view that pulls together our adapter and decorations. We use a lazy implementation took up the columns from resources. This allows us only make the method call once and save the value but using the resources allows us to specify different values for landscape and all other configuration. We could have used a portrait version but this would mean that had a device been square we would not have had a value.
    
    It is also here that we define our spanSizeLookup. This is vital for our desired layout. Here we specify that all Text only recipes should span all columns, while anything else (image tiles) should span only the one.

---
+++ app/src/main/java/com/github/ianellis/recipes/presentation/recipes/recipeslist/RecipesRecycler.kt
@@ -0,0 +1,42 @@
+package com.github.ianellis.recipes.presentation.recipes.recipeslist
+
+import android.content.Context
+import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import com.github.ianellis.recipes.R
+import com.github.ianellis.recipes.domain.getrecipes.Recipe
+
+class RecipesRecycler : RecyclerView {
+
+  private val recipesAdapter = RecipesAdapter(LayoutInflater.from(context))
+
+  private val columns by lazy {
+    resources.getInteger(R.integer.recipes_columns)
+  }
+
+  constructor (context: Context?) : super(context)
+  constructor (context: Context?, attrs: AttributeSet?) : super(context, attrs)
+  constructor (context: Context?, attrs: AttributeSet?, defStyle: Int) : super(context, attrs, defStyle)
+
+  fun setRecipes(recipes:List<Recipe>){
+    recipesAdapter.recipes = recipes
+  }
+
+  init {
+    adapter = recipesAdapter
+    val gridLayout = GridLayoutManager(context,columns)
+    gridLayout.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup(){
+      override fun getSpanSize(position: Int): Int {
+        return when(adapter.getItemViewType(position)){
+          RecipesAdapter.TYPE_TEXT -> columns
+          else -> 1
+        }
+      }
+    }
+    layoutManager = gridLayout
+    addItemDecoration(RecipesSpacingDecoration(columns, resources.getDimensionPixelSize(R.dimen.recipe_spacing)))
+  }
+
+}
